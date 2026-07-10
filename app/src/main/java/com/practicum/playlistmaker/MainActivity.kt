1a1387d584dde042c045333657209395d01deeba
Aleksandr Kostromin <ulis89@yandex.ru>
2026-07-10 17:52:23 +0300

    fix MainActivity.kt

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -3,7 +3,6 @@ package com.practicum.playlistmaker
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
