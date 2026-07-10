513a650e8d6f951fcad76a6def94862d013d43b2
Aleksandr Kostromin <ulis89@yandex.ru>
2026-07-10 17:49:48 +0300

    add tost message on main

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val battonSerch = findViewById<Button>(R.id.button_search)
+        val battonMedia = findViewById<Button>(R.id.button_media)
+        val battonSettings = findViewById<Button>(R.id.button_settings)
+
+        battonSerch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на поиск!", Toast.LENGTH_SHORT).show()
+        }
+        battonMedia . setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на медиатеку!", Toast.LENGTH_SHORT).show()
+        }
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на Настройки но через анонимный класс!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        battonSettings.setOnClickListener(imageClickListener)
     }
 }
