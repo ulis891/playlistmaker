3214139cb8114eb52c3eb1a2910ef04f85c49cc8
Aleksandr Kostromin <ulis89@yandex.ru>
2026-07-12 23:59:46 +0300

    fix compileSdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.practicum.playlistmaker"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
