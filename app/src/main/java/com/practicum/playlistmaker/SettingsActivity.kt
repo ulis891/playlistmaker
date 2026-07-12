cf37b4dba8ff0394b05529fc59a25c4e48e88d62
Aleksandr Kostromin <ulis89@yandex.ru>
2026-07-12 23:15:48 +0300

    add intent from SettingsActivity to MainActivity

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,14 +1,20 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val backArrow = findViewById<Button>(R.id.baсk_arrow)
+
+        backArrow.setOnClickListener{
+            val settingsIntent = Intent(this, MainActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
