912f937bf428544c1b2d996dc8db100174422fd2
Aleksandr Kostromin <ulis89@yandex.ru>
2026-08-21 14:21:26 +0300

    add iTunesAPI

---
+++ app/src/main/java/com/practicum/playlistmaker/iTunesAPI.kt
@@ -0,0 +1,10 @@
+package com.practicum.playlistmaker
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface iTunesAPI {
+    @GET("/search?entity=song")
+    fun getSongs(@Query("term") text: String): Call<Track>
+}
