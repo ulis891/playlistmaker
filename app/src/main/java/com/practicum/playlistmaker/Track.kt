3e0d87605fab5718a65db10615d68c27cac8e669
Aleksandr Kostromin <ulis89@yandex.ru>
2026-08-22 19:42:02 +0300

    fix Track

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -3,6 +3,6 @@ package com.practicum.playlistmaker
 data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
-    val trackTime: String, // Продолжительность трека
+    val trackTime: Int, // Продолжительность трека
     val artworkUrl100: String // Ссылка на изображение обложки
 )
