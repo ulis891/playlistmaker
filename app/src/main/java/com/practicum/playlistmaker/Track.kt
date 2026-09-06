412e399b41bf594980b050dc3c018ce22c8d5099
Aleksandr Kostromin <ulis89@yandex.ru>
2026-09-06 15:44:18 +0300

    add trackID

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -3,6 +3,7 @@ package com.practicum.playlistmaker
 import com.google.gson.annotations.SerializedName
 
 data class Track(
+    val trackId: String,
     val trackName: String,
     val artistName: String,
     @SerializedName("trackTimeMillis") val trackTime: Long,
