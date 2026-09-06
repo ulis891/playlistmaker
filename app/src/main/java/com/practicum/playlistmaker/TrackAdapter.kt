2c647b9b61f94c8a173329b279ff0dd1bcd22b09
Aleksandr Kostromin <ulis89@yandex.ru>
2026-09-06 21:52:31 +0300

    implement click on track

--- app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -5,16 +5,25 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TrackAdapter(
-    private val trackList: List<Track>): RecyclerView.Adapter<TrackViewHolder>() {
+    private val trackList: List<Track>,
+    private val onTrackClick: (Track) -> Unit): RecyclerView.Adapter<TrackViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
             val view = LayoutInflater.from(parent.context).inflate(R.layout.view_track, parent, false)
             return TrackViewHolder(view)
         }
         override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-            holder.bind(trackList[position])
+            val track = trackList[position]
+            holder.bind(track)
+            holder.itemView.setOnClickListener {
+                onTrackClick(track)
+            }
         }
 
         override fun getItemCount(): Int {
             return trackList.size
+        }
+
+        fun isNotEmpty(): Boolean {
+            return trackList.isNotEmpty()
         }
     }
