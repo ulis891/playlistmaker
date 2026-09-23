3b8269bb0610262890b4ca2a8213624f7ae07017
Aleksandr Kostromin <ulis89@yandex.ru>
2026-09-23 20:30:01 +0300

    add fun getCoverArtwork

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -12,4 +12,8 @@ data class Track(
     val releaseDate: String,
     val primaryGenreName: String,
     val country: String
-)
+) {
+    fun getCoverArtwork() = artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
+}
+
+
